0bcca1c597d1d8145496c9b86cfaa56f0d5637ab
Gaurav Goel <grvgoel19@gmail.com>
2023-02-09 13:18:03 +0800

    Adding commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,6 +2,45 @@ import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
 import { multiply, add, getTorusKey} from 'react-native-web3auth-single-factor';
+//import jwt, { Algorithm } from "jsonwebtoken";
+import { sign } from "react-native-pure-jwt";
+
+const jwtPrivateKey = `-----BEGIN PRIVATE KEY-----\nMEECAQAwEwYHKoZIzj0CAQYIKoZIzj0DAQcEJzAlAgEBBCCD7oLrcKae+jVZPGx52Cb/lKhdKxpXjl9eGNa1MlY57A==\n-----END PRIVATE KEY-----`;
+// export const generateIdToken = (email: string, alg: Algorithm) => {
+//   const iat = Math.floor(Date.now() / 1000);
+//   const payload = {
+//     iss: "torus-key-test",
+//     aud: "torus-key-test",
+//     name: email,
+//     email,
+//     scope: "email",
+//     iat,
+//     eat: iat + 120,
+//   };
+
+//   const algo = {
+//     expiresIn: 120,
+//     algorithm: alg,
+//   };
+
+//   return jwt.sign(payload, jwtPrivateKey, algo);
+// };
+
+const idToken = sign(
+  {
+    sub: "email|hello",
+    aud : "torus-key-test",
+    iss: "hello@tor.us",
+    exp: new Date().getTime() + 3600 * 1000, // expiration date, required, in ms, absolute to 1/1/1970
+    additional: "payload"
+  }, // body
+  jwtPrivateKey, // secret
+  {
+    alg: "HS256"
+  }
+)
+  .then(console.log) // token as the only argument
+  .catch(console.error); 
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
@@ -13,7 +52,7 @@ export default function App() {
     getTorusKey('testnet',
     'torus-test-health',
     'hello@tor.us',
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJzdWIiOiJlbWFpbHxoZWxsbyIsImF1ZCI6InRvcnVzLWtleS10ZXN0IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImlzcyI6InRvcnVzLWtleS10ZXN0Iiwibmlja25hbWUiOiJoZWxsbyIsIm5hbWUiOiJoZWxsb0B0b3IudXMiLCJleHAiOjE2NzUyMjAyNjUsImlhdCI6MTY3NTIxNjY2NSwiZW1haWwiOiJoZWxsb0B0b3IudXMiLCJwaWN0dXJlIjoiIn0.yCC-Amj4mg8BfhCQD-1cIdDtx7ItUlE3un95Wh6azDiYtETK-ohX8bOsiwol2ILTCDh3rX69X_0lV1rhcXN5ow')
+    idToken)
     .then(setWeb3AuthResult);
   }, []);
 
