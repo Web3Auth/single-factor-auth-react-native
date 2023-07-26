c659698b2cf9391df5c49aa0b03313cefc3a2100
chaitanyapotti <chaitanya.potti@gmail.com>
2023-07-26 13:57:04 +0800

    add more exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
+export { default as KeyStore } from "./session/KeyStore";
+export * from "./types/IEncryptedStorage";
+export * from "./types/IExpoSecureStore";
 export * from "./types/interface";
 export { default } from "./Web3Auth";
